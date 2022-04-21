2040c79eb3f6721a7e1b3e661e9beb9b6156fa67
Jason Tarasovic <jason@tarasovic.com>
2022-04-21 11:19:14 -0500

    format language server config

--- private_dot_config/nvim/config.lua
+++ private_dot_config/nvim/config.lua
@@ -130,7 +130,13 @@ cmp.setup.cmdline(':', {
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local servers = { 'pyright', 'gopls', 'rust_analyzer', 'terraformls' }
+local servers = {
+    'gopls',
+    'pyright',
+    'rust_analyzer',
+    -- 'sumneko_lua',
+    'terraformls'
+}
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
         capabilities = capabilities,
