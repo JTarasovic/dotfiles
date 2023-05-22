d5b73a5b99f3d27fb7e77d8757699c0dddc0e9a5
Jason Tarasovic <jason@tarasovic.com>
2023-05-22 08:32:42 -0500

    fix: update icons and fix bluetooth block

--- helpers/bluetooth-device-paired.sh
+++ helpers/bluetooth-device-paired.sh
@@ -4,4 +4,4 @@ if ! systemctl is-active bluetooth.service > /dev/null 2>&1; then
     exit 0
 fi
 
-printf "%s" "$(bluetoothctl paired-devices | grep "$1" | cut -d " " -f 2)"
+printf "%s" "$(bluetoothctl devices Paired | grep "$1" | cut -d " " -f 2)"
