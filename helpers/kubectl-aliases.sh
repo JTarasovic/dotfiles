49b045638b3af823cc1b7cd5c9be2323ec666539
Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>
2025-04-25 10:34:30 -0500

    fix: cleanup aliases, funcs and helpers

--- helpers/kubectl-aliases.sh
+++ helpers/kubectl-aliases.sh
@@ -3,8 +3,11 @@
 DELIM=${1:-"-"}
 REPLACE="$2"
 
-for context in $(kubectl config get-contexts -o name);
-do
+for context in $(kubectl config get-contexts -o name); do
+    if expr "$context" : 'kind-.*' >/dev/null; then
+        continue
+    fi
+
     clean_ctx="$(echo "$context" | sed -e "s/$DELIM/$REPLACE/g")"
     alias_to_create="$(printf "%s%s" "k" "$clean_ctx")"
     echo alias "$alias_to_create='kubectl --context=$context '"
